dc21533c009a158fa9b07948884dd6184e326adc
Georgios Karagiannis <georgios.stats@gmail.com>
2015-12-20 17:03:02 -0500

    change 2a

--- rnguniform.f
+++ rnguniform.f
@@ -1,38 +1,38 @@
-! Copyrigtht 2012 Georgios Karagiannis
-!
-! This file is part of AISRJ_Nmix.
-!
-! AISRJ_Nmix is free software: you can redistribute it and/or modify
-! it under the terms of the GNU General Public License as published by
-! the Free Software Foundation version 2 of the License.
-!
-! AISRJ_Nmix is distributed in the hope that it will be useful,
-! but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-! GNU General Public License for more details.
-!
-! You should have received a copy of the GNU General Public License
-! along with AISRJ_Nmix.  If not, see <http://www.gnu.org/licenses/>.
+c Copyrigtht 2012 Georgios Karagiannis
+c
+c This file is part of AISRJ_Nmix.
+c
+c AISRJ_Nmix is free software: you can redistribute it and/or modify
+c it under the terms of the GNU General Public License as published by
+c the Free Software Foundation version 2 of the License.
+c
+c AISRJ_Nmix is distributed in the hope that it will be useful,
+c but WITHOUT ANY WARRANTY; without even the implied warranty of
+c MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+c GNU General Public License for more details.
+c
+c You should have received a copy of the GNU General Public License
+c along with AISRJ_Nmix.  If not, see <http://www.gnu.org/licenses/>.
 
-! ----------------------------------------------------------------------
+c ----------------------------------------------------------------------
 
-! REFERENCES:
-! -----------
-!
-! Karagiannis, G., & Andrieu, C. (2013).
-! Annealed importance sampling reversible jump MCMC algorithms.
-! Journal of Computational and Graphical Statistics, 22(3), 623-648.
-!
-! Georgios Karagiannis
-! School of Mathematics, University of Bristol
-! University Walk, Bristol, BS8 1TW, UK
-! Email : Georgios.Karagiannis@pnnl.gov
-! Email (current): georgios-stats@gmail.com
-!
-! Christophe Andrieu
-! School of Mathematics, University of Bristol
-! University Walk, Bristol, BS8 1TW, UK
-! Email: C.Andrieu@bristol.ac.uk
+c REFERENCES:
+c -----------
+c
+c Karagiannis, G., & Andrieu, C. (2013).
+c Annealed importance sampling reversible jump MCMC algorithms.
+c Journal of Computational and Graphical Statistics, 22(3), 623-648.
+c
+c Georgios Karagiannis
+c School of Mathematics, University of Bristol
+c University Walk, Bristol, BS8 1TW, UK
+c Email : Georgios.Karagiannis@pnnl.gov
+c Email (current): georgios-stats@gmail.com
+c
+c Christophe Andrieu
+c School of Mathematics, University of Bristol
+c University Walk, Bristol, BS8 1TW, UK
+c Email: C.Andrieu@bristol.ac.uk
 
 
       subroutine init_seedrng()
